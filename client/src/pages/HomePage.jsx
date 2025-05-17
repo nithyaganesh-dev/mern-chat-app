03b6e1ecb6c1c9ce6317f0d01bf5711c98f60791
nithyaganesh-dev <nithyaganesh.developer@gmail.com>
2025-05-17 08:13:25 +0530

    Added css for home page layout

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Sidebar from "../components/Sidebar.jsx";
 import ChatContainer from "../components/ChatContainer.jsx";
 import RightSidebar from "../components/RightSidebar.jsx";
 
 const HomePage = () => {
+    const [selectedUser, setSelectedUser] = useState(false)
+
     return (
         <div className='border w-full h-screen sm:p-[15%] sm:py-[5%]'>
-            <div>
+            <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl
+            overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]': 'md:grid-cols-2'}`}>
                 <Sidebar/>
                 <ChatContainer/>
                 <RightSidebar/>
